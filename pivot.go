e9e5ffb98522df738703b2f5bce06f09f517543e
David Hinkes <david.hinkes@gmail.com>
2013-05-13 01:27:21 -0700

    Multiple glob paths possible via command-line.

--- pivot.go
+++ pivot.go
@@ -1,3 +1,5 @@
+// Example usage:
+//  ./pivot --target_directory path/to/my/managed/directory glob/style/path
 package main
 
 import (
@@ -30,29 +32,33 @@ func main() {
 	var targetDirectory = flag.String("target_directory", "", "Directory for processed files.")
 	var test = flag.Bool("test", false, "Just test")
 	flag.Parse()
-	files, _ := filepath.Glob(flag.Args()[0])
-	for _, fn := range files {
-		m, err := extractTiffMetadata(fn)
-		if err == nil {
-			path := filepath.Join(*targetDirectory, m.date)
-			output := filepath.Join(path, m.NewFileName())
-			fmt.Printf("%s -> %s\n", m.originalFileName, output)
-			if !*test {
-				os.MkdirAll(path, 0700)
-				outputFile, _ := os.Create(output)
-				f, _ := os.Open(fn)
-				io.Copy(outputFile, f)
+	for _, glob := range flag.Args() {
+		files, _ := filepath.Glob(glob)
+		for _, fn := range files {
+			m, err := extractTiffMetadata(fn)
+			if err == nil {
+				path := filepath.Join(*targetDirectory, m.date)
+				output := filepath.Join(path, m.NewFileName())
+				fmt.Printf("%s -> %s\n", m.originalFileName, output)
+				if !*test {
+					os.MkdirAll(path, 0700)
+					outputFile, _ := os.Create(output)
+					f, _ := os.Open(fn)
+					io.Copy(outputFile, f)
+				}
 			}
 		}
 	}
 }
+
+const rawTimeFormat = "2006:01:02 15:04:05"
+const pivotDateFormat = "20060102"
 
 func extractTiffMetadata(fn string) (imageMetadata, error) {
 	f, err := os.Open(fn)
 	if err != nil {
 		return imageMetadata{}, err
 	}
-
 	x, err := tiff.Decode(f)
 	if err != nil {
 		return imageMetadata{}, err
@@ -60,12 +66,12 @@ func extractTiffMetadata(fn string) (imageMetadata, error) {
 	for _, dir := range x.Dirs {
 		for _, tag := range dir.Tags {
 			if tag.Id == 306 {
-				t, err := time.Parse("2006:01:02 15:04:05", tag.StringVal())
+				t, err := time.Parse(rawTimeFormat, tag.StringVal())
 				if err != nil {
 					return imageMetadata{}, err
 					log.Fatal(err)
 				}
-				dir := t.Format("20060102")
+				dir := t.Format(pivotDateFormat)
 				h := crypto.SHA256.New()
 				io.WriteString(h, fn)
 				newFileName := hex.EncodeToString(h.Sum(nil))
